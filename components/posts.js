cecc24a7f984f5be7c815a5c3f33022d114fd199
Vipin Sahu <vipinsahu1990@gmail.com>
2024-05-02 17:34:08 +0530

    caching

--- components/posts.js
+++ components/posts.js
@@ -1,7 +1,23 @@
-import { formatDate } from '@/lib/format';
-import LikeButton from './like-icon';
-
+import { formatDate } from "@/lib/format";
+import LikeButton from "./like-icon";
+import { useOptimistic } from "react";
 function Post({ post }) {
+  const [optimisticPosts, updateOpstimisticPost] = useOptimistic(
+    post,
+    (prevPost, updatedPostId) => {
+      const updatedPostIndex = prevPost.findIndex(
+        (post) => post.id === updatedPostId
+      );
+      if (updatedPostIndex === -1) {
+        return prevPost;
+      }
+      const updatedPost = { ...prevPost[updatedPostIndex] };
+      updatedPost.likes = updatedPost.likes + (updatedPost.isLiked ? -1 : 1);
+      updatedPost.isLiked = !updatedPost.isLiked;
+      const newPost = [...prevPost];
+      newPost[updatedPostIndex] = updatedPost;
+    }
+  );
   return (
     <article className="post">
       <div className="post-image">
@@ -12,7 +28,7 @@ function Post({ post }) {
           <div>
             <h2>{post.title}</h2>
             <p>
-              Shared by {post.userFirstName} on{' '}
+              Shared by {post.userFirstName} on{" "}
               <time dateTime={post.createdAt}>
                 {formatDate(post.createdAt)}
               </time>
@@ -29,13 +45,13 @@ function Post({ post }) {
 }
 
 export default function Posts({ posts }) {
-  if (!posts || posts.length === 0) {
+  if (!optimisticPosts || optimisticPosts.length === 0) {
     return <p>There are no posts yet. Maybe start sharing some?</p>;
   }
 
   return (
     <ul className="posts">
-      {posts.map((post) => (
+      {optimisticPosts.map((post) => (
         <li key={post.id}>
           <Post post={post} />
         </li>
